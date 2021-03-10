627c90cac85ba5bfd1c25c5c411c026770ae5fc0
Asar5 <asara@oregonstate.edu>
2021-03-10 11:48:15 -0600

    Fixed jerky motion in simulation by removing step inside step

--- Manipulator.py
+++ Manipulator.py
@@ -123,6 +123,13 @@ class Manipulator(SceneObject):
 
         return contact_points
 
+    def step_sim(self, move_to_joint_poses):
+        p.stepSimulation()
+        time.sleep(1. / 240.)
+        p.setJointMotorControlArray(bodyIndex=self.gripper_id, jointIndices=self.joint_indices,
+                                    controlMode=p.POSITION_CONTROL, targetPositions=move_to_joint_poses)
+
+
     def check_for_contact(self, cube):
         """
         MAKES SURE CUBE IS IN contact with  hand
@@ -133,8 +140,6 @@ class Manipulator(SceneObject):
         in_contact = False
         for i in range(0, tot_attempts):
             target_pos =  []
-            p.stepSimulation()
-            time.sleep(1. / 240.)
             contact_points = self.get_contact_points(cube.object_id)
             if None in contact_points:
                 # maintain contact
@@ -145,8 +150,7 @@ class Manipulator(SceneObject):
                 pose_for_contact = p.calculateInverseKinematics2(bodyUniqueId=self.gripper_id,
                                                                  endEffectorLinkIndices=self.end_effector_indices,
                                                                  targetPositions=target_pos)
-                p.setJointMotorControlArray(bodyIndex=self.gripper_id, jointIndices=self.joint_indices,
-                                            controlMode=p.POSITION_CONTROL, targetPositions=pose_for_contact)
+                self.step_sim(pose_for_contact)
 
             else:
                 in_contact = True
@@ -166,8 +170,6 @@ class Manipulator(SceneObject):
         done = False
 
         while p.isConnected() and not done:
-            p.stepSimulation()
-            time.sleep(1. / 240.)
             # Query joint angles and check if they match or are close to where we want them to be
             done = self.pose_reached(joint_poses, abs_tol)
             if contact_check:
@@ -175,16 +177,15 @@ class Manipulator(SceneObject):
                 try:
                     in_contact = self.check_for_contact(cube)
                     print("IN CONTACT? {}".format(in_contact))
-                    p.setJointMotorControlArray(bodyIndex=self.gripper_id, jointIndices=self.joint_indices,
-                                                controlMode=p.POSITION_CONTROL, targetPositions=joint_poses)
+                    self.step_sim(joint_poses)
 
                 except EnvironmentError:
                     print("We Have Lost Contact with Object. Aborting Mission")
                     setup.quit_sim()
 
             else:
-                p.setJointMotorControlArray(bodyIndex=self.gripper_id, jointIndices=self.joint_indices,
-                                            controlMode=p.POSITION_CONTROL, targetPositions=joint_poses)
+                self.step_sim(joint_poses)
+
         if cube is not None:
             contact_points = self.get_contact_points(cube.object_id)
 
